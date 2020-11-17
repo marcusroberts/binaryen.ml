3cb32285cd02bc99fd1d6db1607f3ee8443883d0
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-11-16 23:52:07 -0700

    maybe do not reuse the local dir

--- windows-setup.sh
+++ windows-setup.sh
@@ -1,4 +1,4 @@
 ROOT="$(cygpath -m /)"
-LOCAL_PACKAGE_DIR="$(cygpath -w /var/cache/setup)"
+LOCAL_PACKAGE_DIR="$(cygpath -w /var/cache/update)"
 
 /setup-x86_64.exe --root $ROOT -q --packages=cmake,python3 --local-package-dir $LOCAL_PACKAGE_DIR --site=http://cygwin.mirror.constant.com/ --no-desktop --no-startmenu --no-shortcuts --upgrade-also
