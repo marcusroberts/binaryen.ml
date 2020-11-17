4d60d3b39a420e2ad40634cbe0e32472b20fa8b2
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-11-17 12:38:57 -0700

    no gitattributes?

--- windows-setup.sh
+++ windows-setup.sh
@@ -1,6 +1,4 @@
 ROOT="$(cygpath -m /)"
 LOCAL_PACKAGE_DIR="$(cygpath -w /var/cache/setup)"
 
-/setup-x86_64.exe --root $ROOT -q --packages=cmake,python3,mingw64-x86_64-gcc-core,mingw64-x86_64-gcc-g++,mingw64-x86_64-headers,mingw64-x86_64-runtime,mingw64-x86_64-winpthreads --local-package-dir $LOCAL_PACKAGE_DIR --site=http://cygwin.mirror.constant.com/ --no-desktop --no-startmenu --no-shortcuts
-
-cat /var/log/setup.log.full
+/setup-x86_64.exe --root $ROOT -q --packages=cmake,python3,mingw64-x86_64-gcc-core,mingw64-x86_64-gcc-g++,mingw64-x86_64-headers,mingw64-x86_64-runtime,mingw64-x86_64-winpthreads --local-package-dir $LOCAL_PACKAGE_DIR --site=http://cygwin.mirror.constant.com/ --no-desktop --no-startmenu --no-shortcuts --verbose
