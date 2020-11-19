8acbaa6899e71d9e990753ecba05e5a07f9ab198
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-11-19 11:23:42 -0700

    less packages

--- windows-setup.sh
+++ windows-setup.sh
@@ -1,4 +1,4 @@
 ROOT="$(cygpath -m /)"
 LOCAL_PACKAGE_DIR="$(cygpath -w /var/cache/setup)"
-PACKAGES="cmake,python3,mingw64-x86_64-binutils,mingw64-x86_64-winpthreads,mingw64-x86_64-headers,mingw64-x86_64-runtime,mingw64-x86_64-windows-default-manifest,mingw64-x86_64-gcc-core,mingw64-x86_64-gcc-g++"
-/setup-x86_64.exe --root $ROOT -q --packages $PACKAGES --local-package-dir $LOCAL_PACKAGE_DIR --site=http://cygwin.mirror.constant.com/ --no-desktop --no-startmenu --no-shortcuts --verbose
+PACKAGES="cmake,python3"
+$ROOT/setup-x86_64.exe --root $ROOT -q --packages $PACKAGES --local-package-dir $LOCAL_PACKAGE_DIR --site=http://cygwin.mirror.constant.com/ --no-desktop --no-startmenu --no-shortcuts --verbose
